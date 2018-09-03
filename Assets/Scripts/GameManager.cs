84cc2b4f109e10aad120fd0fa6153c5a1196ad15
Sahapat <sahapat.starcaft@gmail.com>
2018-09-03 13:14:10 +0700

    Create Gamemanger script and setting GameCore for globa

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameCore
+{
+    public static GameManager gameManager;
+}
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+		GameCore.gameManager = this;
+    }
+}
