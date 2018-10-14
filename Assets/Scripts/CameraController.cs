de873cf74abc35843d3e9cd4c0688e85a98d6103
Sahapat <sahapat.starcaft@gmail.com>
2018-10-14 23:00:17 +0700

    UpdateCamera MoveFocus

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class CameraController:MonoBehaviour
 {
+    [Header("Reference")]
+    [SerializeField] private GameObject targetPlayer;
+    [SerializeField] private Vector3 moveLeft;
+    [SerializeField] private Vector3 moveRight;
+    [SerializeField] private Vector3 moveUp;
+    [SerializeField] private Vector3 moveDown;
     private Vector3 moveFocus = new Vector3(60,0,-10f);
     private void Update()
     {
-        transform.position = moveFocus;
-    }
-    public void SetMoveFocus(Vector3 position)
-    {
-        moveFocus = new Vector3(position.x,position.y,-10f);
+        var temp = Camera.main.WorldToViewportPoint(targetPlayer.transform.position);
+        if(temp.y < 0)
+        {
+            transform.position += moveDown;
+        }
+        else if(temp.y > 1)
+        {
+            transform.position += moveUp;
+        }
+        else if(temp.x < 0)
+        {
+            transform.position += moveLeft;
+        }
+        else if(temp.x > 1)
+        {
+            transform.position += moveRight;
+        }
     }
 }
